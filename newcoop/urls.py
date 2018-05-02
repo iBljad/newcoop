a154d04accba7fc9f103cfeca7ce4949f51d3f7b
iBljad <drakonmail@gmail.com>
2018-05-02 23:52:35 +0300

    initial commit +django-allauth +postgresql configured

---
+++ newcoop/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('newcoop_app.urls')),
+    path('admin/', admin.site.urls),
+    url(r'^accounts/', include('allauth.urls')),
+]
