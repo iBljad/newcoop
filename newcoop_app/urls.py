761b7f051aefdd49c8ba5e46e9638a5005d722dd
iBljad <drakonmail@gmail.com>
2018-05-06 02:01:42 +0300

    request post logic added generic view for request detail comment field to post form added

--- newcoop_app/urls.py
+++ newcoop_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'newcoop_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('request/<int:gamer_request_id>', views.request_detail, name='request_detail_name'),
+    path('request/<int:pk>', views.RequestDetailsView.as_view(), name='request_detail'),
     path('request_post', views.request_post, name='request_post_name'),
 ]
--- newcoop_app/views.py
+++ newcoop_app/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
 
 from .models import *
 
@@ -11,12 +14,46 @@ def index(request):
     return render(request, 'nca/index.html', {'games': games, 'platforms': platforms, 'languages': languages})
 
 
-def request_detail(request, gamer_request_id):
-    game_request = get_object_or_404(GameRequest, pk=gamer_request_id)
-    rating = round(
-        10 * game_request.requestlikes_set.filter(liked=True).count() / game_request.requestlikes_set.count(), 2)
-    return render(request, 'nca/request_detail.html', {'game_request': game_request, 'rating': rating})
+# def request_detail(request, gamer_request_id):
+#     game_request = get_object_or_404(GameRequest, pk=gamer_request_id)
+#     likes = game_request.requestlikes_set.filter(liked=True).count()
+#     # round(10 * game_request.requestlikes_set.filter(liked=True).count() / game_request.requestlikes_set.count(), 2)
+#     return render(request, 'nca/request_detail.html', {'game_request': game_request, 'likes': likes})
+
+
+class RequestDetailsView(generic.DetailView):
+    model = GameRequest
+    template_name = 'nca/request_detail.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['likes'] = self.get_object().requestlikes_set.filter(liked=True).count()
+        return context
 
 
 def request_post(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    try:
+        game = Game.objects.get(pk=request.POST['game'])
+        platform = Platform.objects.get(pk=request.POST['platform'])
+        language = Language.objects.get(pk=request.POST['language'])
+        mic = request.POST.get('mic', False)
+        comment = request.POST.get('comment', '')
+        user = request.user
+    except KeyError:
+        messages.add_message(request, messages.ERROR, 'Something went wrong')
+        return HttpResponseRedirect(reverse('newcoop_app:index'))
+    else:
+        if user.is_authenticated:
+            game_request = GameRequest(game=game, platform=platform, language=language,
+                                       mic_present=True if mic is not False else False, comment=comment,
+                                       user=request.user)
+            game_request.save()
+            messages.add_message(request, messages.SUCCESS, 'Your request successfully created')
+            return HttpResponseRedirect(reverse('newcoop_app:request_detail', args=(game_request.id,)))
+        else:
+            messages.add_message(request, messages.WARNING, 'You are not authenticated, please log in')
+            return HttpResponseRedirect(reverse('newcoop_app:index'))
+
+    # return HttpResponse("Hello, world. You're at the polls index.")
